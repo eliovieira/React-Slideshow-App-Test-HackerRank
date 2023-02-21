64e9671e38b0f61c01cf201ae3f1ca33d196f13e
Élio Vieira <eliovieira@iMac-de-Elio.home>
2023-02-21 11:18:34 +0000

    last commit

--- src/components/Slides.js
+++ src/components/Slides.js
@@ -1,23 +1,41 @@
 import React from "react";
+import { useState } from "react";
 
 function Slides({ slides }) {
+  const [activeSlideId, setActiveSlideId] = useState(0);
+
   return (
     <div>
       <h1>teste</h1>
       <div id="navigation" className="text-center">
-        <button data-testid="button-restart" className="small outlined">
+        <button
+          data-testid="button-restart"
+          className="small outlined"
+          onClick={() => setActiveSlideId(0)}
+        >
           Restart
         </button>
-        <button data-testid="button-prev" className="small">
+
+        <button
+          data-testid="button-prev"
+          className="small"
+          onClick={() => setActiveSlideId(activeSlideId - 1)}
+          disabled={activeSlideId === 0}
+        >
           Prev
         </button>
-        <button data-testid="button-next" className="small">
+        <button
+          data-testid="button-next"
+          className="small"
+          onClick={() => setActiveSlideId(activeSlideId + 1)}
+          disabled={activeSlideId === slides.length - 1}
+        >
           Next
         </button>
       </div>
       <div id="slide" className="card text-center">
-        <h1 data-testid="title">Slide Title Here</h1>
-        <p data-testid="text">Slide Text Here</p>
+        <h1 data-testid="title">{slides[activeSlideId].title}</h1>
+        <p data-testid="text">{slides[activeSlideId].text}</p>
       </div>
     </div>
   );
